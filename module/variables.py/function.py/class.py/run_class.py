a992159a877770d980c8bfc9c914ef7eb4de3e71
DESKTOP-0LB2K8R\yoshinori1 <nattawan.w@ku.th>
2021-02-20 16:57:43 +0700

    new creat files is class_module_function

---
+++ module/variables.py/function.py/class.py/run_class.py
@@ -0,0 +1,12 @@
+import class_person
+
+name = "Nichada Thongmaim"
+age = 20
+address = "Bangkok"
+
+person = class_person.Person(name, age, address)
+
+print(person.get_name())
+print(person.get_age())
+print(person.get_address())
+print(person.get_info())
---
+++ module/variables.py/variables.py
@@ -0,0 +1,2 @@
+person = {"name": "Yoshi", "age": 22, "country": "Korean"}
+person1 = {"name": "Asahi", "age": 21, "country": "Korean"}
